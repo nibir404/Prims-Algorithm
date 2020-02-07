3644415fa826a1a792958ee5a0e69df469b53351
Nibir Imtiaz <nibirimtiaz1@gmail.com>
2020-02-07 12:06:06 +0600

    Add files via upload

---
+++ Prim's Algo/src/MST.java
@@ -0,0 +1,58 @@
+public class MST {
+    private static final int V = 5;
+    int minKey(int key[],Boolean mstSet[]){
+        int min = Integer.MAX_VALUE,min_index = -1;
+
+        for (int v = 0 ; v < V; v++)
+            if (mstSet[v] == false && key[v] < min){
+                min = key[v];
+                min_index = v;
+            }
+        return min_index;
+    }
+
+    void printMST(int parent[],int graph[][]){
+        System.out.println("Edge \tWeight");
+        for (int i = 1; i<V ; i++)
+            System.out.println(parent[i]+" - "+i+"\t" + graph[i][parent[i]]);
+    }
+
+    void primMST(int graph[][]){
+        int parent [] = new int[V];
+
+        int key [] = new int[V];
+
+        Boolean mstSet[] = new Boolean[V];
+
+        for (int i =0 ; i<V ; i++){
+            key[i] = Integer.MAX_VALUE;
+            mstSet[i] = false;
+        }
+
+        key[0] = 0;
+        parent[0] = -1;
+
+        for (int count = 0; count < V-1; count++){
+            int u = minKey(key,mstSet);
+            mstSet[u]= true;
+            for (int v=0 ; v<V;v++)
+                if (graph[u][v] !=0 && mstSet[v]== false && graph[u][v] < key[v]){
+                    parent[v] = u;
+                    key[v] = graph[u][v];
+                }
+        }
+
+        printMST(parent,graph);
+    }
+
+    public static void main(String[] args) {
+        MST t = new MST();
+        int graph[][] = new int[][]{{0,2,0,6,0},
+                {2,0,4,6,7},
+                {0,4,5,3,8},
+                {5,6,4,7,3},
+                {0,5,3,2,6}};
+        t.primMST(graph);
+      }
+    
+}
